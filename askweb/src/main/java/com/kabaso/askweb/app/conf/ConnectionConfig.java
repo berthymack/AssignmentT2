6c0d1dee2684f64973b75800875950c6ba2a0122
hashcode <hashcode@dev>
2014-04-15 09:39:50 +0200

    Added the Person Respository and a Contact Emeddable

--- askweb/src/main/java/com/kabaso/askweb/app/conf/ConnectionConfig.java
+++ askweb/src/main/java/com/kabaso/askweb/app/conf/ConnectionConfig.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.kabaso.askweb.app.conf;
 
 import javax.sql.DataSource;
@@ -27,35 +26,38 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "com.kabaso.askweb.respository")
 public class ConnectionConfig {
-    
-@Bean
-	public DataSource dataSource() {
-		BasicDataSource ds = new org.apache.commons.dbcp.BasicDataSource();
-		ds.setDriverClassName("org.apache.derby.jdbc.ClientDriver");
-		ds.setUrl("jdbc:derby://localhost:1527/sample");
-		ds.setUsername("app");
-		ds.setPassword("app");
-		return ds;
-	}
-	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-			DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
-		LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
-		lef.setDataSource(dataSource);
-		lef.setJpaVendorAdapter(jpaVendorAdapter);
-		lef.setPackagesToScan("com.kabaso.askweb.domain");
-		return lef;
-	}
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-		hibernateJpaVendorAdapter.setShowSql(false);
-		hibernateJpaVendorAdapter.setGenerateDdl(true);
-		hibernateJpaVendorAdapter.setDatabase(Database.DERBY);
-		return hibernateJpaVendorAdapter;
-	}
-	@Bean
-	public PlatformTransactionManager transactionManager() {
-		return new JpaTransactionManager();
-	}
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new org.apache.commons.dbcp.BasicDataSource();
+        ds.setDriverClassName("org.apache.derby.jdbc.ClientDriver");
+        ds.setUrl("jdbc:derby://localhost:1527/sample");
+        ds.setUsername("app");
+        ds.setPassword("app");
+        return ds;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        LocalContainerEntityManagerFactoryBean lef = new LocalContainerEntityManagerFactoryBean();
+        lef.setDataSource(dataSource);
+        lef.setJpaVendorAdapter(jpaVendorAdapter);
+        lef.setPackagesToScan("com.kabaso.askweb.domain");
+        return lef;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+        hibernateJpaVendorAdapter.setShowSql(false);
+        hibernateJpaVendorAdapter.setGenerateDdl(true);
+        hibernateJpaVendorAdapter.setDatabase(Database.DERBY);
+        return hibernateJpaVendorAdapter;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
 }
---
+++ askweb/src/test/java/com/kabaso/askweb/test/respository/PersonRepositoryTest.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.kabaso.askweb.test.respository;
+
+import com.kabaso.askweb.app.conf.ConnectionConfig;
+import com.kabaso.askweb.domain.Contact;
+import com.kabaso.askweb.domain.Customer;
+import com.kabaso.askweb.domain.Person;
+import com.kabaso.askweb.respository.CustomerRepository;
+import com.kabaso.askweb.respository.PersonRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author boniface
+ */
+
+public class PersonRepositoryTest {
+    public static ApplicationContext ctx;
+    private Long id;
+
+    private PersonRepository repo;
+    
+    public PersonRepositoryTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+     public void createPerson() {
+         repo = ctx.getBean(PersonRepository.class);
+         Person p = new Person();
+         p.setEmail("kabaso@cput.ac.za");
+         p.setFirstname("B");
+         p.setLastname("K");
+         Contact c = new Contact();
+         c.setAddress("p.O.Box 13");
+         c.setCell("2424253");
+         c.setLandline("12345");
+         p.setContact(c);
+         repo.save(p);
+         Assert.assertNotNull(p);
+         
+     
+     }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        	 ctx = new AnnotationConfigApplicationContext(ConnectionConfig.class);
+		
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
